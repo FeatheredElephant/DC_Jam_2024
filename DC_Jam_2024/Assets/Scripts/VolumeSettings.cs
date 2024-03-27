3f4ac28cfb52954d1261f8c080e2a7551032f969
Anaya Potter <nyaari01@outlook.com>
2024-03-27 17:26:21 -0400

    Updated main menu UI & added sounds
    Added
    - Controls Panel
    - Credits Panel
    - SFX volume slider
    - Sounds on button click
    - Downloaded sounds
    Deleted
    - Main Menu prefab

--- DC_Jam_2024/Assets/Scripts/VolumeSettings.cs
+++ DC_Jam_2024/Assets/Scripts/VolumeSettings.cs
@@ -6,10 +6,18 @@ public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
-    
+    [SerializeField] private Slider SFXSlider;
+
+
     public void SetMusicVol()
     {
         float vol = musicSlider.value;
-        myMixer.SetFloat("music", vol);
+        myMixer.SetFloat("music", Mathf.Log10(vol)*20);
+    }
+
+    public void SetEffectsVol()
+    {
+        float SFXvol = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(SFXvol)*20);
     }
 }
